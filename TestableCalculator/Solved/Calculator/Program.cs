28c19381cb7386d4f3f8397cef772e9c480923f3
JirkaPok <j.pok@post.cz>
2019-06-12 23:43:58 +0000

    Refactor the Calculate method to simplify the code

--- TestableCalculator/Solved/Calculator/Program.cs
+++ TestableCalculator/Solved/Calculator/Program.cs
@@ -9,30 +9,20 @@ namespace Calculator
 			int first = Convert.ToInt32(Console.ReadLine());
 			int second = Convert.ToInt32(Console.ReadLine());
 			var command = (Commands)Enum.Parse(typeof(Commands), Console.ReadLine());
-
-            int result;
-            
-            result = Calculate(command, first, second);
-
+            var result = Calculate(command, first, second);
             Console.WriteLine(result);
-
-
             Console.ReadKey();
         }
 
         internal static int Calculate(Commands command, int first, int second)
         {
-            int result;
             switch (command)
             {
                 case Commands.add:
-                    result = Mathematics.Add(first, second);
-                    break;
+                    return Mathematics.Add(first, second);
                 default:
                     throw new InvalidOperationException("Unknown command in calculator");
             }
-
-            return result;
         }
     }
 }
