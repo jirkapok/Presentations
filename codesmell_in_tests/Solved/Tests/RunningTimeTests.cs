a35b2fb697493ebbfefbe9d5dddd0fae25eaa7e7
JirkaPok <j.pok@post.cz>
2020-10-13 05:55:33 -0700

    Implemented fix in running time in test smells

--- codesmell_in_tests/Solved/Tests/RunningTimeTests.cs
+++ codesmell_in_tests/Solved/Tests/RunningTimeTests.cs
@@ -7,14 +7,14 @@ namespace Tests
     public class RunningTimeTests
     {
         // execute this test multiple times to see its results
-        // code smell:
+        // code smell fixed:
         // - DateTime.Now depends on time zone
         // - the test mimics the implementation 
         [Test]
         public void Always_Use_Fixed_Values()
         {
-            var source = DateTime.Now;
-            var expected = source.ToShortTimeString();
+            var source = new DateTime(2000, 9, 12,14,36,41, DateTimeKind.Utc);
+            var expected = "2:36:41 PM";
             var formatted = RunningTime.FormatToReport(source);
             Assert.That(formatted, Is.EqualTo(expected));
         }
