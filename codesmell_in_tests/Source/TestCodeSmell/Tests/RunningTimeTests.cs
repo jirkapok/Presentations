060dd83a8e93b0c7a41372dc5a7da250415f22f6
JirkaPok <j.pok@post.cz>
2020-10-13 03:36:37 -0700

    Added example for multiple asserts in test code smell

--- codesmell_in_tests/Source/TestCodeSmell/Tests/RunningTimeTests.cs
+++ codesmell_in_tests/Source/TestCodeSmell/Tests/RunningTimeTests.cs
@@ -6,11 +6,13 @@ namespace Tests
 {
     public class RunningTimeTests
     {
+        // execute this test multiple times to see its results
+        // code smell:
+        // - DateTime.Now depends on time zone
+        // - the test mimics the implementation 
         [Test]
         public void Always_Use_Fixed_Values()
         {
-            // execute this test multiple times to see its results
-            // there are two mistakes: DateTime.Now depends on time zone and the test mimics the implementation 
             var source = DateTime.Now;
             var expected = source.ToShortTimeString();
             var formatted = RunningTime.FormatToReport(source);
