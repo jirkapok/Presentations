8c2664537e32f0d7e2b596f4f2c39b1cfc131118
JirkaPok <j.pok@post.cz>
2020-10-13 06:07:21 -0700

    Implemented multiple asserts test smells

--- codesmell_in_tests/Solved/Tests/MultipleAssertTests.cs
+++ codesmell_in_tests/Solved/Tests/MultipleAssertTests.cs
@@ -1,12 +1,14 @@
 ﻿using NUnit.Framework;
 using System;
+using FluentAssertions;
+using NUnit.Framework.Interfaces;
 
 namespace Tests
 {
     [TestFixture]
     public class MultipleAssertTests
     {
-        // code smell:
+        // code smell fixed:
         // - custom assert method has lot of arguments
         // - when first assert fails we don't know the results of following asserts, solved tricky by assert message
         // - useless assert message
@@ -14,20 +16,20 @@ namespace Tests
         public static void Ctor_Long_DateTimeKind()
         {
             var sut = new DateTime(999999999999999999, DateTimeKind.Utc);
-            VerifyDateTime(sut, 3169, 11, 16, 19, 46, 39, 999, DateTimeKind.Utc);
-        }
 
-        private static void VerifyDateTime(DateTime dateTime, int year, int month, int day, int hour, int minute, int second, int millisecond, DateTimeKind kind)
-        {
-            Assert.That(year, Is.EqualTo(dateTime.Year), "#1");
-            Assert.That(month, Is.EqualTo(dateTime.Month), "#2");
-            Assert.That(day, Is.EqualTo(dateTime.Day), "#3");
-            Assert.That(hour, Is.EqualTo(dateTime.Hour), "#4");
-            Assert.That(minute, Is.EqualTo(dateTime.Minute), "#5");
-            Assert.That(second, Is.EqualTo(dateTime.Second), "#6");
-            Assert.That(millisecond, Is.EqualTo(dateTime.Millisecond), "#7");
-
-            Assert.That(kind, Is.EqualTo(dateTime.Kind));
+            // cast ot object to avoid implicit DateTime comparison for test purposes
+            // compare the error message with original code
+            ((object)sut).Should().BeEquivalentTo(
+            new {
+                Year = 3169,
+                Month = 11,
+                Day = 16,
+                Hour = 19,
+                Minute = 46,
+                Seconds = 39,
+                Millisecond = 999,
+                Kind = DateTimeKind.Utc
+            });
         }
     }
 }
