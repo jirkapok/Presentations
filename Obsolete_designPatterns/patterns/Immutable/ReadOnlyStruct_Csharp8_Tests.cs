a337916152c50c6e0219c26a21ab4e8ff6665a39
JirkaPok <j.pok@post.cz>
2023-01-27 07:27:33 -0800

    Extracted record examples

--- Obsolete_designPatterns/patterns/Immutable/ReadOnlyStruct_Csharp8_Tests.cs
+++ Obsolete_designPatterns/patterns/Immutable/ReadOnlyStruct_Csharp8_Tests.cs
@@ -13,17 +13,6 @@ public class ReadOnlyStruct_Csharp8_Tests
     }
 
     [Test]
-    public void ReadOnlyRecordStruct()
-    {
-        var sut = new ReadOnlyRecordStruct("bbb");
-        var sut2 = new ReadOnlyRecordStruct() { Name = "bbb" };
-        // can't sut.Name = "aaa";
-
-        Assert.That(sut.Name, Is.EqualTo("bbb"));
-        Assert.That(sut2.Name, Is.EqualTo("bbb"));
-    }
-
-    [Test]
     public void ReadOnlyPropertyStruct()
     {
         var sut = new ReadOnlyPropertyStruct("abc");
---
+++ Obsolete_designPatterns/patterns/Immutable/Records_Csharp9_Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace patterns.Immutable;
+
+[TestFixture]
+public class Records_Csharp9_Tests
+{
+
+    [Test]
+    public void ReadOnlyRecordStruct()
+    {
+        var sut = new ReadOnlyRecordStruct("bbb");
+        var sut2 = new ReadOnlyRecordStruct() { Name = "bbb" };
+        // can't sut.Name = "aaa";
+
+        Assert.That(sut.Name, Is.EqualTo("bbb"));
+        Assert.That(sut2.Name, Is.EqualTo("bbb"));
+    }
+
+    [Test]
+    public void WithKeyWord_Record()
+    {
+        var record1 = new ReadOnlyRecordStruct("aaa");
+        var record2 = record1 with { Name = "bbb" };
+        Assert.That(record1.Name, Is.EqualTo("aaa"));
+        Assert.That(record2, Is.EqualTo(new ReadOnlyRecordStruct("bbb")));
+    }
+}
