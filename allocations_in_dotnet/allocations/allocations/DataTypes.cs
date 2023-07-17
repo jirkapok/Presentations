c05dc621946bff4398cccb9a07bf32a000eb7227
JirkaPok <j.pok@post.cz>
2023-07-17 14:09:55 -0700

    added more allocation todos

--- allocations_in_dotnet/allocations/allocations/DataTypes.cs
+++ allocations_in_dotnet/allocations/allocations/DataTypes.cs
@@ -4,10 +4,12 @@ public static class Datatypes
 {
     public static void Run()
     {
+        // take snapshot, see locals to observe current stack frame initial values of the variables
         var cA = new ClassA { P1 = 1 };
         var sA = new StructA { P1 = 2 };
         var cB = new ClassA { P1 = 3 };
         var sB = new StructA { P1 = 4 };
+        // take second snapshot and observe heap
     }
 }
 
--- allocations_in_dotnet/allocations/allocations/Program.cs
+++ allocations_in_dotnet/allocations/allocations/Program.cs
@@ -8,6 +8,15 @@
 Datatypes.Run();
 
 
+// TODO add more examples:
+// compare: class, struct, class record, struct record ( > C# 10)
+// https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/records
+// pointers
+// array allocations
+// string allocations
+// compare large object heap and pinned objects
+// typical scenarios
+
 // Based on following:
 // https://benchmarkdotnet.org/
 // https://plugins.jetbrains.com/plugin/9223-heap-allocations-viewer
