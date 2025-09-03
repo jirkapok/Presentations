59ab81edd26fe527418bd6ecb3efb479135d7b0a
Jiri Pokorny <jiri.pokorny@mews.com>
2025-09-03 21:00:10 +0200

    More functional program class

--- FunctionalCsharp/Calculator/Program.cs
+++ FunctionalCsharp/Calculator/Program.cs
@@ -2,15 +2,15 @@
 
 WriteResult(
     Calculate(
-        ReadNumber("Enter first number:"),
-        ReadNumber("Enter second number:"),
-        ReadText("Enter mathematical operation (+,-,*,/): ")));
+        () => ReadNumber("Enter first number:"),
+        () => ReadNumber("Enter second number:"),
+        () => ReadText("Enter mathematical operation (+,-,*,/): ")));
 
-int Calculate(int first, int second, string operation) => operation switch
+int Calculate(Func<int> getFirst, Func<int> getSecond, Func<string> getOperation) => getOperation() switch
 {
-    "+" => first + second,
-    "-" => first - second,
-    "*" => first * second,
-    "/" => first / second,
+    "+" => getFirst() + getSecond(),
+    "-" => getFirst() - getSecond(),
+    "*" => getFirst() * getSecond(),
+    "/" => getFirst() / getSecond(),
     _ => throw new NotImplementedException("Unknown mathematical operation")
 };
