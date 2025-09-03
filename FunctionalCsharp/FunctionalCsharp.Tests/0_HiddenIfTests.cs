870970650afa1a523023dcf3c0db2cad593fe154
Jiri Pokorny <jiri.pokorny@mews.com>
2025-09-03 02:11:23 +0200

    Removed Either initialization

--- FunctionalCsharp/FunctionalCsharp.Tests/0_HiddenIfTests.cs
+++ FunctionalCsharp/FunctionalCsharp.Tests/0_HiddenIfTests.cs
@@ -12,21 +12,20 @@ public class HiddenIfTests
     [Test]
     public void Conditional_resolves_value()
     {
-        var items = Either<string, int>.Right(7);
-        var result = FormatItemsCount(items);
+        var result = FormatItemsCount(Option<int>.Some(7)); // implicit conversion to Either<string, Option<int>>
 
-        Assert.That(result, Is.EqualTo("Items 7"));
+        Assert.That(result, Is.EqualTo("Items 9"));
     }
 
     // How many tests do i need for this method?
-    private static string FormatItemsCount(Either<string, int> count)
+    private static string FormatItemsCount(Either<string, Option<int>> count)
     {
         var offset = 2;
 
         return count
             .Match(
-                Right: Option<int>.Some,
-                Left: s => int.Parse(s))
+                Right: r => r,
+                Left: l => int.Parse(l))
             .Match(
                 Some: v => $"Items {v + offset}", // allocation
                 None: () => "No items");
